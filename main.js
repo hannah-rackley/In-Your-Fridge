57b9ed4c90a5a8eb8c6475f7c39eaa9cf60b976f
Brandon Humphries <brandon.j.humphries@gmail.com>
2018-08-28 08:07:46 -0400

    user can now enter email and password to signup, user email and password are captured

--- main.js
+++ main.js
@@ -3,3 +3,15 @@ closeLoginWindow.addEventListener('click', () => {
     let loginModalWindow = document.querySelector('.login-modal-container');
     loginModalWindow.classList.add('hidden');
 });
+
+
+let submitSignupInformation = document.querySelector('.signup-form');
+submitSignupInformation.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let userEmail = document.querySelector('.signup-email-input');
+    let userPassword = document.querySelector('.signup-password-input');
+    console.log(userEmail.value);
+    console.log(userPassword.value);
+    let loginModalWindow = document.querySelector('.login-modal-container');
+    loginModalWindow.classList.add('hidden');
+});
