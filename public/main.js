64e3af7798cc4cb1e77fb29cf78ab38818f76b5e
Hannah <hrglasser94@gmail.com>
2018-09-03 13:49:00 -0400

    Added edge cases into the submit Login function to check for the wrong username(as in one that does not exist in our database) or the wrong password(if the username does exist but the pw does not match). Added ability to display error messages in login menu.

--- public/main.js
+++ public/main.js
@@ -296,6 +296,10 @@ let submitLoginInfo = (event) => {
     captureUserCredentials('login');
     let credentials = captureUserCredentials('login');
     clearUserInformationInput('login');
+    let wrongPass = document.querySelector('.wrong-password');
+    wrongPass.classList.add('hidden');
+    let wrongUser = document.querySelector('.wrong-login-info');
+    wrongUser.classList.add('hidden');
     fetch('/tokens', {
         method: 'POST',
         body: JSON.stringify(credentials),
@@ -303,10 +307,24 @@ let submitLoginInfo = (event) => {
     }).then(results => {
         return results.text()})
         .then(text => {
-            localStorage.setItem("token", JSON.stringify(text))
-            getStaples();
-            displayUserEmail(credentials.email);
-            loginButtonStatus();
+            if (text === "Wrong login information") {
+                clearUserInformationInput('login');
+                let wrongUser = document.querySelector('.wrong-login-info');
+                wrongUser.classList.remove('hidden');
+                let loginContainer = document.querySelector('.login-modal-container');
+                loginContainer.classList.remove('hidden');
+            } else if (text === "Wrong password") {
+                clearUserInformationInput('login');
+                let loginContainer = document.querySelector('.login-modal-container');
+                loginContainer.classList.remove('hidden');
+                let wrongPass = document.querySelector('.wrong-password');
+                wrongPass.classList.remove('hidden');
+            } else {
+                localStorage.setItem("token", JSON.stringify(text))
+                getStaples();
+                displayUserEmail(credentials.email);
+                loginButtonStatus();
+            }
         });
 };
 
